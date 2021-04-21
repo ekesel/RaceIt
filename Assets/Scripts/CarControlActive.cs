db197125ea09921887047c8293cf90fc38e76a3d
Ekaansh Sahni <ekaanshsahni@gmail.com>
2021-04-21 09:46:49 +0530

    Full game

---
+++ Assets/Scripts/CarControlActive.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class CarControlActive : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject CarControl;
+    public GameObject Dreamcar01;
+    void Start()
+    {
+     	CarControl.GetComponent<CarController>().enabled = true;
+	Dreamcar01.GetComponent<CarAIControl>().enabled = true;   
+    }
+}
